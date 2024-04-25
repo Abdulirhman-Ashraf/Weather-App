cb5e137f512c79b9446ea256f268bf47b20d8ff5
Abdulirhman-Ashraf <153239328+Abdulirhman-Ashraf@users.noreply.github.com>
2024-04-25 22:36:10 +0200

    Add files via upload

--- master.js
+++ master.js
@@ -1,13 +1,19 @@
-
 let apiBase = `https://api.openweathermap.org/data/2.5/weather?q=`;
 let apiKey = "5e950be15af49b475695991a409bc6aa";
 async function get(city) {
-  const response = await fetch(
-    `${apiBase}${city}&appid=${apiKey}&lang=PT&units=metric`
-  );
-  const data = await response.json();
-  document.querySelector(
-    `h3`
-  ).innerHTML = `weather in ${city}  ${data.main["temp"]} °C `;
-
+  if (city != "") {
+    const response = await fetch(
+      `${apiBase}${city}&appid=${apiKey}&lang=PT&units=metric`
+    );
+    const data = await response.json();
+    console.log(data);
+    let c = data.main["temp"];
+    let cloud = data.weather[0].main;
+    let icon = data.weather[0].icon;
+    let icons = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.querySelector(
+      `h3`
+    ).innerHTML = `<img src= ${icons}><br> ${Math.round(c)} °C<br> ${cloud} `;
+  } else {
+  }
 }
